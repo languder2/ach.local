da20c5669e639d5e158c230ad3ba365df5e60e60
languder2 <163087417+languder2@users.noreply.github.com>
2024-08-13 16:46:11 +0300

    asd

--- public_html/js/user.js
+++ public_html/js/user.js
@@ -3,15 +3,34 @@ document.addEventListener("DOMContentLoaded",()=>{
     if(signUp !== null){
         signUp.addEventListener("submit",(evt)=>{
             evt.preventDefault();
-            let formData= new FormData(signUp);
-            let required= signUp.querySelectorAll("input[required]:not([value=''])");
 
-            if(formData.get("form[password]") != formData.get("form[retry]")){
-                console.log("no retry");
+            let required= signUp.querySelectorAll("input[required]");
+
+            required.forEach(el=>{
+                if(el.value === '')
+                    el.classList.add("is-invalid");
+                else
+                    el.classList.remove("is-invalid");
+            });
+
+            let formData= new FormData(signUp);
+
+            if(validateEmail(formData.get("form[email]")) === null){
+                console.log("email null");
+                signUp.querySelector("#suEmail").classList.add("is-invalid");
+                return false;
             }
 
-
+            if(formData.get("form[password]") !== formData.get("form[retry]")){
+                console.log("pass != retry");
+            }
         });
     }
 });
-
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
